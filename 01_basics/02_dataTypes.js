ffd231e4463d6318611ada4b1d3537b83e684c5e
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-09-16 21:30:49 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Vikram")
+
+let name = "Vikram"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined); //undefined
