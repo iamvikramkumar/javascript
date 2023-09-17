511dbc38edd6211bb6aa8e1e181fc2dd15a3ea28
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-09-17 20:52:39 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+ let score ="33abc"
+// let score =true
+// let score ="Vikram"
+// let score = null
+
+
+//"33" => 33
+//"33abc" => NaN -- Not a Number
+// true => 1; false => 0
+
+// let isLoggedIn = "Vikram"  //true
+let isLoggedIn = ""  //false
+
+//1 => true; 0 => false
+// "" => false
+//"Vikram" => true
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
