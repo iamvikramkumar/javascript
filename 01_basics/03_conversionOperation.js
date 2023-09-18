5eb076792f154c3d290c0020a52d6f6c24af205d
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-09-18 20:16:08 +0000

    updated conversion operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,5 +29,40 @@ let isLoggedIn = ""  //false
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********* Operations ********* 
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) 2 to power 2  = 4
+// console.log(2/3)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Vikram"
+
+let str3 = str1 + str2
+// console.log(str3)
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
