3c1b13ce62050a00037c987dfaf9bb2c6b5ba21f
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-09-20 09:15:40 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "vikram"
+const repoCount = 22
+
+// console.log(name + repoCount + " Value"); //outdated
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  //Modern way
+
+//Declaration of string 
+
+const gameName = new String ('vikramvl')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
