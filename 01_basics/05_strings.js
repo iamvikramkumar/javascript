882a5f524be16c31becf2ed9a5bb9931e3e958ab
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-09-21 19:50:28 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,8 +7,29 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  //Mo
 
 //Declaration of string 
 
-const gameName = new String ('vikramvl')
-console.log(gameName[0])
-console.log(gameName.__proto__)
+const gameName = new String ('vikram-vl')
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
 
-console.log(gameName.length)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('v'));
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString)
+
+const newStringOne = " vikram     "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://vikram.com/vikram%20Kumar"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('rohit'))
+
+console.log(gameName.split('-'))
