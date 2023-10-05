6378e7ae4ed835fc0f74609d2c9d5287336e8887
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-10-05 15:23:04 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -10,20 +10,30 @@ const JsUser = {
     "full name": "Vikram Kumar",
     [mySym] : "myKey1",
     age: 20,
-    locatio: "Muzaffarpur",
+    location: "Muzaffarpur",
     email: "vikram@google.com",
     isLoggedIn: false,
     lastLoginDays:["Monday", "Saturday"] 
 }
 
-console.log(JsUser.email)
-console.log(JsUser["email"])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
 
-console.log(JsUser["full name"])
-console.log( JsUser[mySym])
+// console.log(JsUser["full name"])
+// console.log( JsUser[mySym])
 
 JsUser.email = "vikram@paytm.com"
-Object.freeze(JsUser)
+// Object.freeze(JsUser) // if we don't want change something email by someone then we have to fixed that using freeze 
 JsUser.email = "Vikram@microsoft.com"
+// console.log(JsUser)
 
-console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this["full name"]}`);
+}
+console.log(JsUser.greetingTwo()); 
+console.log(JsUser.greeting());
