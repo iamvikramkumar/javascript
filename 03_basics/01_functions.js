b301ded157757b7155359459a7af6f5603492315
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-10-06 20:02:15 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -45,4 +45,33 @@ function loginUserMessage(username = "sam"){
 
 // console.log(loginUserMessage("vikram"));
 // console.log(loginUserMessage(""));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "vikram",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 1000, 2000]));
