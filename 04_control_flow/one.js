22b0778b50f49ce23760579b4fbd39097bfc4789
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-10-12 20:28:48 +0000

    control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,40 @@
+// if
+const isUserloggedIn = true
+
+// const temperature = 41
+
+// if(temperature === 41){
+//     console.log("less than 50");
+// }else{
+// console.log("temperature is greater than 50");}
+
+// if(2 != 3){
+//     console.log("executed")
+// }
+
+// 2<=2
+// 3 != 2
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+//const balance = 1000
+// if(balance > 500) console.log("test"), console.log("test2"); //implicit scope --> ye ek hi line me execute hota hai
+
+// if(balance < 500){
+//     console.log("less than");
+// } else if(balance < 750){
+//     console.log("less than 750");
+// } else if(balance < 900){
+//     console.log("less than 750");
+// } else{
+//     console.log("less than 1200");
+// }
