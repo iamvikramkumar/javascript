4babee6c4f95f0149b49a4713d017afeff82b337
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-10-13 21:01:27 +0000

    one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -51,3 +51,19 @@ if(userLoggedIn && debitCard && 2==2){
 if(loggedInFromGoogle || loggedInFromEmail ){
     console.log("User logged in");
 }
+
+//Nullish Coalescing Operator (??) null undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+// val1 = undefined ?? 15
+//val1 = null ? 10 ?? 20
+//console.log(val1);
+
+ // Ternary Operator
+
+ // conditon ? true : false
+
+ const iceTeaPrice = 100
+ iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
