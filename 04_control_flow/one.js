600df0455fdafbe7950a7f0fe6d1f74cf2131d0b
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-10-13 20:22:16 +0000

    one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -38,3 +38,16 @@ const isUserloggedIn = true
 // } else{
 //     console.log("less than 1200");
 // }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail ){
+    console.log("User logged in");
+}
