d7c466254f934f399c2b26576232143bf5782bb6
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-10-20 16:36:54 +0000

    forin_or_forof

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -8,6 +8,22 @@ const myObject = {
 for (const key in myObject) {
     // console.log(myObject[key]);
 
-    console.log(`${key} shortcut is for ${myObject[key]}`);
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+const programming = ["js", "rb", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('BR', "BHARAT")
+// map.set('Fr',"France")
+// map.set('Ru', "Russia")
+// map.set("BR", "BHARAT")
+
+// for (const key in map) {
+//     console.log(key);
+// }
 
